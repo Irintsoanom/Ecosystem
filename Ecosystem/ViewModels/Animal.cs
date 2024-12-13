5de535d493011accc0cde11dbce366b096b1ac29
Irintsoanom <22336@ecam.be>
2024-12-13 13:38:18 +0100

    Adding animal class

---
+++ Ecosystem/ViewModels/Animal.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ecosystem.ViewModels;
+
+public partial class Animal : LivingCreature
+{
+    [ObservableProperty]
+    private int viewArea;
+    [ObservableProperty]
+    private int contactZone;
+    [ObservableProperty]
+    private string sex;
+
+    public Animal(Point location) : base(location)
+    {
+        this.sex = "Male";
+    }
+
+    private void Move()
+    {
+
+    }
+    private void Defecate()
+    {
+
+    }
+
+}
