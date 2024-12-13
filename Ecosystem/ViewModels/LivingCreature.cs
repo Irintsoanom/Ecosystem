856dc476124589aea7710db8ed9ede509b8e0730
Irintsoanom <22336@ecam.be>
2024-12-13 13:25:47 +0100

    LivingCreature base

---
+++ Ecosystem/ViewModels/LivingCreature.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ecosystem.ViewModels;
+
+public partial class LivingCreature : GameObject
+{
+    [ObservableProperty]
+    private int lifePoint;
+    [ObservableProperty]
+    private int energyReserve;
+    [ObservableProperty]
+    private Point startingPoint = new Point(1.0, 0);
+
+    public LivingCreature(Point location):base(location) {
+        
+    }
+
+    private void Feed()
+    {
+
+    }
+    private void Reproduce()
+    {
+
+    }
+}
