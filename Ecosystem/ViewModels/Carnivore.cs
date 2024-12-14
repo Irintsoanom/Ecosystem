3cae85fa1758532988a22839b56ee025376ec2cc
Irintsoanom <22336@ecam.be>
2024-12-14 10:29:14 +0100

    Carnivore strucutre

---
+++ Ecosystem/ViewModels/Carnivore.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ecosystem.ViewModels;
+
+public partial class Carnivore: Animal
+{
+    public Carnivore(Point location) : base(location) 
+    { 
+        
+    }
+}
