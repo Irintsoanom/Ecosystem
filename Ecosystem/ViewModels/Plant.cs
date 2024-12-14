d2e03e8c666aadf544326f6c4824fdc0966d4997
Irintsoanom <22336@ecam.be>
2024-12-14 09:54:39 +0100

    Plant structure

---
+++ Ecosystem/ViewModels/Plant.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ecosystem.ViewModels;
+
+public partial class Plant : LivingCreature
+{
+    [ObservableProperty]
+    private int rootArea;
+    [ObservableProperty]
+    private int seedZone;
+    public Plant(Point point) : base(point)
+    {
+
+    }
+}
