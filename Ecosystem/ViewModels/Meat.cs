b325bcbaa2f5eb069d2fc68acdf491d8c723c257
Irintsoanom <22336@ecam.be>
2024-12-21 11:35:38 +0100

    Adding Meat and Organic waste class

---
+++ Ecosystem/ViewModels/Meat.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Metadata;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Timers;
+
+namespace Ecosystem.ViewModels;
+
+public partial class Meat
+{
+    private Timer decayTimer = new Timer(3000);
+    public Meat()
+    {
+        this.decayTimer.Elapsed += OnTimerElapsed;
+    }
+    private void Decay()
+    {
+
+    }
+    private void OnTimerElapsed(object? sender, EventArgs e)
+    {
+        Decay();
+    }
+}
